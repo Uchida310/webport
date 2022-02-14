25f91b4c03b463a35f17bb8014994bb13f1f67a6
Uchida310 <mitsuya.uchida310@gmail.com>
2022-02-14 22:04:03 +0900

    iframe搭載/DOM要素追加/js変数メソッド編集/不要ファイル削除

--- js/app.js
+++ js/app.js
@@ -3,17 +3,25 @@ const app = () => {
     const play = document.querySelector('.play'); //playクラスを取得
 
     const outline = document.querySelector('.moving-outline circle') //svgに設定したクラス
-    const video = document.querySelector('.video-container video');
-
     const sounds = document.querySelectorAll('.sound-picker button');
+    const studyDisplay = document.querySelector('.study-display');
+    const intervalDisplay = document.querySelector('.interval-display');
+
     const timeDisplay = document.querySelector('.time-display');
     const timeSelect = document.querySelectorAll('.time-select button');
     //円の長さを取得・・・約1359
     const outlineLength = outline.getTotalLength();
-    //console.log(outlineLength);
 
     //曲の再生間隔
     let fakeDuration = 600;
+
+    //勉強時間
+
+
+    //
+    //合計時間
+    let sumTime = 0;
+    
 
     //strokeDasharray・・・svgのプロパティ　間隔を指定
     outline.style.strokeDasharray = outlineLength;
@@ -23,61 +31,43 @@ const app = () => {
 
     //クリックされたら関数を実行
     play.addEventListener('click', () => {
-        checkPlaying(song);
+        play.src = './svg/pause.svg';
     });
 
     //再生サウンドを変更
-    sounds.forEach(sound => {
-        sound.addEventListener('click', function(){
-            song.src = this.getAttribute('data-sound');
-            video.src = this.getAttribute('data-video');
-            checkPlaying(song);
-        });
-    });
+
 
     timeSelect.forEach( Option => {
         Option.addEventListener('click', function() {
             fakeDuration = this.getAttribute('data-time');
+            sumTime += this.getAttribute('data-time');
             timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:
                                         ${Math.floor(fakeDuration % 60)}`;
+            console.log(sumTime);
         });
     });
     
 
-    //音楽が再生されいるか否かで判定する
-    const checkPlaying = song => {
-        if(song.paused){
-            song.play();
-            video.play();
-            play.src = './svg/pause.svg';
-        }else{
-            song.pause();
-            video.pause();
-            play.src = './svg/play.svg';
-        }
-    }
 
     
-    song.ontimeupdate = () => { //曲が再生されている間だけ実行される
-        let currentTime = song.currentTime;
-        let elapsed = fakeDuration - currentTime;
-        let seconds = Math.floor(elapsed % 60);
-        let minutes = Math.floor(elapsed / 60);
+    // song.ontimeupdate = () => { //曲が再生されている間だけ実行される
+    //     let currentTime = sumTime;
+    //     let elapsed = fakeDuration - currentTime;
+    //     let seconds = Math.floor(elapsed % 60);
+    //     let minutes = Math.floor(elapsed / 60);
     
-        //円のアニメーション
-        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-        outline.style.strokeDashoffset = progress;
+    //     //円のアニメーション
+    //     let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    //     outline.style.strokeDashoffset = progress;
 
-        timeDisplay.textContent = `${minutes}:${seconds}`;
+    //     timeDisplay.textContent = `${minutes}:${seconds}`;
 
-        if(currentTime >= fakeDuration) {
-            song.pause();
-            song.currentTime = 0;
-            play.src = './svg/play.svg';
-            video.pause();
-        }
+    //     if(currentTime >= fakeDuration) {
+    //         song.currentTime = 0;
+    //         play.src = './svg/play.svg';
+    //     }
 
-    }
+    // }
     
 
 }
